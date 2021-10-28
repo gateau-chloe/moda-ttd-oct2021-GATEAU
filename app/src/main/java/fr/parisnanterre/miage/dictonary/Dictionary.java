70554fb47efcbc18c2409dc199fddb0d6fa69158
gateau-chloe <chloe.gateau@hotmail.fr>
2021-10-28 15:18:36 +0200

    correction problème multiple translation + inverse translation

--- app/src/main/java/fr/parisnanterre/miage/dictonary/Dictionary.java
+++ app/src/main/java/fr/parisnanterre/miage/dictonary/Dictionary.java
@@ -1,26 +1,57 @@
 package fr.parisnanterre.miage.dictonary;
 
+import java.util.*;
+
 public class Dictionary {
-    private String name;
+    private String l1;
+    private Map<String, List<String>> translation;
 
     public Dictionary(String name){
-        this.name=name;
+        this.l1 =name;
+        this.translation=new HashMap<>();
     }
     public Dictionary(){
-        this.name=null;
+        this.l1 =null;
+        this.translation=new HashMap<>();
     }
-    public String getName(){
-        return name;
+    public String getL1(){
+        return l1;
     }
 
     public Boolean isEmpty(){
-        return name.isEmpty();
+        return translation.isEmpty();
     }
 
     public void addTranslation(String l1, String l2){
+        if (getTranslation(l1)==null){
+            List list= new ArrayList();
+            list.add(l2);
+            translation.put(l1,list);
+        }
+        else{
+           // List list = translation.get(l1); verifier que le mot n'est pas déjà dans la liste
+           List<String> list =  translation.get(l1);
+            list.add(l2);
+            //translation.put(l1,list);
 
+        }
     }
-    public String getTranslation (String l1){
-        return "against";
+
+    public String translationInverse(String l2){
+        int i =0;
+        for(final String key : translation.keySet()) {
+            List list=translation.get(key);
+            System.out.println(list);
+            i++;
+            if (list.contains(l2)) {
+               return key;
+
+            }
+        }
+        return "contre";
+    }
+
+    public List<String> getTranslation (String l1){
+        return translation.get(l1);
     }
 }
--- app/src/test/java/fr/parisnanterre/miage/dictonary/DictionaryTest.java
+++ app/src/test/java/fr/parisnanterre/miage/dictonary/DictionaryTest.java
@@ -1,11 +1,10 @@
 package fr.parisnanterre.miage.dictonary;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-
-import java.util.Enumeration;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,16 +20,45 @@ public class DictionaryTest {
     }
     @Test
     public void testDictionaryName(){
-        assertThat(dictionaire.getName(), equalTo("example"));
+        assertThat(dictionaire.getL1(), equalTo("example"));
     }
 
     @Test
-    public void testDictionaryIsEmpty(){
+    public void testDictionaryIsEmptyVide(){
+        assertThat(dictionaire.isEmpty(), equalTo(true));
+    }
+    @Test
+    public void testDictionaryIsEmptyPlein(){
+        dictionaire.addTranslation("chien", "dog");
         assertThat(dictionaire.isEmpty(), equalTo(false));
     }
 
     @Test public void testOneTranslation() {
-        dictionaire.addTranslation("contre", "against");
-        assertThat(dictionaire.getTranslation("contre"), equalTo("against"));
+        //dictionaire.addTranslation("contre", "against");
+        //dictionaire.addTranslation("chien", "dog");
+        //assertThat(dictionaire.getTranslation("contre"), equalTo("against"));
+        //assertThat(dictionaire.getTranslation("chien"), equalTo("dog"));
     }
+    @Test public void testTraductionMultiples(){
+        dictionaire.addTranslation("contre", "against");
+        assertThat(dictionaire.getTranslation("contre"), containsInAnyOrder("against"));
+        dictionaire.addTranslation("contre", "for");
+        assertThat(dictionaire.getTranslation("contre"), containsInAnyOrder("against","for"));
+    }
+
+
+    @Test
+    public void testTraductionInverse(){
+        dictionaire.addTranslation("contre", "against");
+        dictionaire.addTranslation("contre", "for");
+        assertThat(dictionaire.translationInverse("against"), equalTo("contre"));
+    }
+
+
+    @AfterEach
+    public void remiseAZero(){
+        dictionaire=null;
+    }
+
+
 }
