fbf4e24a38d8961024c7c068e3db57d2cc583eda
gateau-chloe <chloe.gateau@hotmail.fr>
2021-10-28 15:24:38 +0200

    ajout allTest

---
+++ app/src/test/java/fr/parisnanterre/miage/dictonary/AllTest.java
@@ -0,0 +1,12 @@
+package fr.parisnanterre.miage.dictonary;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DictionaryTest.class,
+})
+public class AllTest {
+    // Empty class ( introspection )
+}
